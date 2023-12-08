2b7d799fb65d04a5fec85ec3a710feb388e07292
김준호 <kjhlove0119@naver.com>
2023-12-08 11:41:04 +0900

    Revert "Revert "Merge branch 'main' into Chong""
    This reverts commit cb75b8fb1e96c18172b536d4930950b045d7c2b1.

--- 2023_winapi_framework/Capybara.cpp
+++ 2023_winapi_framework/Capybara.cpp
@@ -4,20 +4,31 @@
 #include "ResMgr.h"
 #include "Animator.h"
 #include "Animation.h"
+#include "Collider.h"
+#include "Texture.h"
 #include "Core.h"
 
 Capybara::Capybara()
-	: texture(nullptr)
+	: texture{}
 	, isDrag(false)
+	, isStand(false)
+	, isStage(false)
+	, isAttatched(false)
 	, dragStartPos{}
 {
-	texture = ResMgr::GetInst()->TexLoad(L"Capybara", L"Texture\\CapybaraSheet.bmp");
+	texture[0] = ResMgr::GetInst()->TexLoad(L"Capybara", L"Texture\\CapybaraSheet.bmp");
+	texture[1] = ResMgr::GetInst()->TexLoad(L"Stage", L"Texture\\stage.bmp");
+
+	Vec2 vPos = GetPos();
 
 	CreateAnimator();
-	GetAnimator()->CreateAnim(L"Capybara_idle", texture, Vec2(0.f, 0.f),
+	GetAnimator()->CreateAnim(L"Capybara_idle", texture[0], Vec2(0.f, 0.f),
 		Vec2(512.f, 512.f), Vec2(512.f, 0.f), 1, 0.2f);
-	GetAnimator()->CreateAnim(L"Capybara_Stand", texture, Vec2(512.f, 0.f), Vec2(512.f, 512.f), Vec2(512.f, 0.f), 1, 1.f);
-	GetAnimator()->PlayAnim(L"Capybara_idle", true);
+	GetAnimator()->CreateAnim(L"Capybara_Stand", texture[0], Vec2(512.f, 0.f), Vec2(512.f, 512.f), Vec2(512.f, 0.f), 1, 1.f);
+
+	CreateCollider();
+	GetCollider()->SetScale(Vec2(512.f * 0.1f, 256.f * 0.2f));
+	GetCollider()->SetOffSetPos(Vec2(vPos.x + 10, vPos.y + 20));
 
 }
 
@@ -28,31 +39,112 @@ Capybara::~Capybara()
 
 void Capybara::Update()
 {
-	GetAnimator()->Update();
+	if (isStage) {
 
-	Vec2 vPos = GetPos();
-	POINT mousePos = KeyMgr::GetInst()->GetMousePos();
+	}
+	else {
+		if (!isStand) {
+			GetAnimator()->PlayAnim(L"Capybara_idle", true);
+			GetAnimator()->Update();
 
-	if (IsClickAble(this) && !Core::GetInst()->isDragging) {
-		if (KEY_DOWN(KEY_TYPE::LBUTTON)) {
-			isDrag = true;
-			Core::GetInst()->isDragging = true;
-			dragStartPos = vPos;
+			Vec2 vPos = GetPos();
+			POINT mousePos = KeyMgr::GetInst()->GetMousePos();
+			
+			if (IsClickAble(this) && !Core::GetInst()->isDragging) {
+				if (KEY_DOWN(KEY_TYPE::LBUTTON)) {
+					isDrag = true;
+					Core::GetInst()->isDragging = true;
+					dragStartPos = vPos;
+				}
+			}
+			if (KEY_UP(KEY_TYPE::LBUTTON) && isDrag) {
+				Core::GetInst()->isDragging = false;
+				isDrag = false;
+			}
+
+			if (isDrag) {
+				vPos = (Vec2)mousePos;
+			}
+
+			SetPos(vPos);
+
 		}
 	}
-	if (KEY_UP(KEY_TYPE::LBUTTON) && isDrag) {
-		Core::GetInst()->isDragging = false;
-		isDrag = false;
-	}
-
-	if (isDrag) {
-		vPos = (Vec2)mousePos;
-	}
-
-	SetPos(vPos);
 }
 
 void Capybara::Render(HDC _dc)
 {
-	Component_Render(_dc);
+	if (isStage) {
+		Vec2 vPos = GetPos();
+		Vec2 vScale = GetScale();
+		Vec2 vScaleOffset = GetScaleOffset();
+		int Width = texture[1]->GetWidth();
+		int Height = texture[1]->GetHeight();
+
+		HDC alphaDC = CreateCompatibleDC(_dc);
+		HBITMAP alphabit = CreateCompatibleBitmap(_dc, Core::GetInst()->GetResolution().x, Core::GetInst()->GetResolution().y);
+		SelectObject(alphaDC, alphabit);
+
+		PatBlt(alphaDC, 0, 0, Core::GetInst()->GetResolution().x, Core::GetInst()->GetResolution().y, WHITENESS);
+
+		StretchBlt(alphaDC
+			, 0
+			, 0
+			, Width * vScaleOffset.x
+			, Height * vScaleOffset.y
+			, texture[1]->GetDC()
+			, 0
+			, 0
+			, Width
+			, Height
+			, SRCCOPY);
+
+		TransparentBlt(_dc
+			, (int)(vPos.x - Height * vScaleOffset.x / 2.f)
+			, (int)(vPos.y - Width * vScaleOffset.y / 2.f)
+			, Width * vScaleOffset.x
+			, Height * vScaleOffset.y
+			, alphaDC
+			, 0
+			, 0
+			, Width * vScaleOffset.x
+			, Height * vScaleOffset.y
+			, RGB(255, 0, 255));
+
+		Component_Render(_dc);
+		DeleteDC(alphaDC);
+		DeleteObject(alphabit);
+	}
+	else
+		Component_Render(_dc);
+}
+
+void Capybara::EnterCollision(Collider* _pOther)
+{
+	Capybara* obj = (Capybara*)_pOther->GetObj();
+	
+	Vec2 contactVec = obj->GetCollider()->GetFinalPos();
+	Vec2 thisVec = GetCollider()->GetFinalPos();
+	
+	Vec2 calcVec = (thisVec - contactVec).Normalize();
+
+	if (obj->GetIsStand()) {
+		if (calcVec.y < -0.7f && !obj->GetIsAttatched()) {
+			isStand = true;
+			obj->SetIsAttatched(true);
+		}
+		else {
+			return;
+		}
+	}
+}
+
+void Capybara::StayCollision(Collider* _pOther)
+{
+	if (isStage) return;
+	GetAnimator()->PlayAnim(L"Capybara_Stand", true);
+	if (isStand) {
+		Core::GetInst()->isDragging = false;
+		isDrag = false;
+	}
 }
--- 2023_winapi_framework/PenguinScene.cpp
+++ 2023_winapi_framework/PenguinScene.cpp
@@ -19,7 +19,7 @@ void PenguinScene::Init()
 		int x = resolution.x / 2;
 		int y = resolution.y / 2;
 
-		penguin->SetPos(Vec2(x + 200, y + 230 - (i * 60)));
+		penguin->SetPos(Vec2(x + 180, y + 230 - (i * 60)));
 		penguin->SetScale(Vec2(512.f * 0.2f, 512.f * 0.2f));
 		penguin->SetScaleOffset(Vec2(0.2f, 0.2f));
 
@@ -36,11 +36,14 @@ void PenguinScene::Update()
 		if (!obj->isClick)
 			return;
 	}
-	SceneMgr::GetInst()->LoadScene(L"Stage_1");
+	SceneMgr::GetInst()->LoadScene(L"Stage_6");
 }
 
 void PenguinScene::Render(HDC _dc)
 {
+	Vec2 vPos = Vec2(Core::GetInst()->GetResolution());
+	Vec2 vScale = Vec2(450, 620);
+	RECT_RENDER(vPos.x / 2, vPos.y / 2 + 20, vScale.x, vScale.y, _dc);
 	Scene::Render(_dc);
 }
 
