d6473f1c2812131bd2445478376fcf1788413d62
김준호 <kjhlove0119@naver.com>
2023-12-13 15:23:40 +0900

    StageEnd
    dd

--- 2023_winapi_framework/Sloth.h
+++ 2023_winapi_framework/Sloth.h
@@ -1,20 +1,22 @@
 #pragma once
 #include "Object.h"
 class Texture;
-class Wood :
+class Sloth :
     public Object
 {
 public:
-    Wood();
-    ~Wood();
+    Sloth();
+    ~Sloth();
 public:
     void Update() override;
     void Render(HDC _dc) override;
 public:
     void EnterCollision(Collider* _pOther) override;
 public:
-    bool isSlothWood;
+    bool isHang;
 private:
     Texture* texture;
+    bool isDrag;
+    Vec2 dragStartPos;
 };
 
---
+++ 2023_winapi_framework/Toucan.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+#include "Toucan.h"
+#include "ResMgr.h"
+#include "Texture.h"
+#include "KeyMgr.h"
+#include "TimeMgr.h"
+#include "Animator.h"
+#include "Animation.h"
+
+Toucan::Toucan() : texture(nullptr), isOpenBeak(false), openTime(1.f)
+{
+    // 텍스처 받아오기
+    texture = ResMgr::GetInst()->TexLoad(L"Toucan.bmp", L"Texture\\Toucan.bmp");
+
+    // 애니메이션 세팅
+    CreateAnimator();
+    GetAnimator()->CreateAnim(L"Toucan_CloseBeak", texture, Vec2(0.f, 0.f), Vec2(512.f, 512.f), Vec2(512.f, 0.f), 1, 5.f);
+    GetAnimator()->CreateAnim(L"Toucan_OpenBeak", texture, Vec2(512.f, 0.f), Vec2(512.f, 512.f), Vec2(512.f, 0.f), 1, 5.f);
+}
+
+Toucan::~Toucan()
+{
+
+}
+
+void Toucan::Update()
+{
+    openTime += fDT;
+
+    if (IsClickAble(this)) // 클릭 됐는가
+    {
+        if (KEY_DOWN(KEY_TYPE::LBUTTON)) // 좌측 마우스 버튼으로 클릭했는가
+        {
+            GetAnimator()->PlayAnim(L"Toucan_OpenBeak", false);
+            isOpenBeak = true;
+            openTime = 0;
+        }
+    }
+
+    if (openTime >= 1.f)
+    {
+        GetAnimator()->PlayAnim(L"Toucan_CloseBeak", false);
+        isOpenBeak = false;
+    }
+}
+
+void Toucan::Render(HDC _dc)
+{
+    Component_Render(_dc);
+}
