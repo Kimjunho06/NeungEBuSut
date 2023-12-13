76ec68a743ef28d6732d2e568aca7bf0a21c508d
김준호 <kjhlove0119@naver.com>
2023-12-13 13:58:01 +0900

    dd

--- 2023_winapi_framework/SceneMgr.cpp
+++ 2023_winapi_framework/SceneMgr.cpp
@@ -9,6 +9,8 @@
 #include "SeaOtterScene.h"
 #include "PenguinScene.h"
 #include "CapybaraScene.h"
+#include "KangarooScene.h"
+#include "SlothScene.h"
 
 void SceneMgr::Init()
 {
@@ -24,9 +26,8 @@ void SceneMgr::Init()
 	RegisterScene(L"Stage_5", std::make_shared<PenguinScene>());
 	RegisterScene(L"Stage_6", std::make_shared<CapybaraScene>());
 	RegisterScene(L"Stage_7", std::make_shared<ChameleonScene>());
-	RegisterScene(L"Stage_8", std::make_shared<ChameleonScene>());
-	RegisterScene(L"Stage_9", std::make_shared<ChameleonScene>());
-	RegisterScene(L"Stage_10", std::make_shared<ChameleonScene>());
+	RegisterScene(L"Stage_8", std::make_shared<KangarooScene>());
+	RegisterScene(L"Stage_9", std::make_shared<SlothScene>());
 	
 	// Ã¹ ¾À ÁöÁ¤
 	LoadScene(L"Stage_8"); //
